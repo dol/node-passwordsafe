435ef8ba0dc277cf1f0d3cd17b306494a7dadd8b
Dominic Luechinger <doldev@snowgarden.ch>
2014-11-01 16:00:50 +0100

    Renamed test variable in load callback

--- test/read.js
+++ test/read.js
@@ -7,9 +7,9 @@ describe('Loading a database', function() {
     describe('without providing any credentials', function() {
         it('should trigger an error', function(done) {
             var safe = new PasswordSafe();
-            safe.load(psafe3Data, function(err, result) {
+            safe.load(psafe3Data, function(err, databaseRecords) {
                 should.exist(err);
-                should.not.exist(result);
+                should.not.exist(databaseRecords);
                 err.should.be.exactly('Wrong password provided.');
                 done();
             });
@@ -21,9 +21,9 @@ describe('Loading a database', function() {
             var safe = new PasswordSafe({
                 password: 'invalidPassword',
             });
-            safe.load(psafe3Data, function(err, result) {
+            safe.load(psafe3Data, function(err, databaseRecords) {
                 should.exist(err);
-                should.not.exist(result);
+                should.not.exist(databaseRecords);
                 err.should.be.exactly('Wrong password provided.');
                 done();
             });
@@ -33,9 +33,9 @@ describe('Loading a database', function() {
     describe('empty input buffer', function() {
         it('should trigger an database format error', function(done) {
             var safe = new PasswordSafe();
-            safe.load(new Buffer(0), function(err, result) {
+            safe.load(new Buffer(0), function(err, databaseRecords) {
                 should.exist(err);
-                should.not.exist(result);
+                should.not.exist(databaseRecords);
                 err.should.be.exactly('Invalid database format.');
                 done();
             });
@@ -56,18 +56,18 @@ describe('Loading the test database', function() {
         var safe = new PasswordSafe({
             password: '123456'
         });
-        safe.load(psafe3Data, function(err, result) {
+        safe.load(psafe3Data, function(err, databaseRecords) {
             should.not.exist(err);
-            should.exist(result);
+            should.exist(databaseRecords);
 
-            result.should.be.instanceof(Array);
-            result.should.have.keys(
+            databaseRecords.should.be.instanceof(Array);
+            databaseRecords.should.have.keys(
                 'ba8bd21f-2ce9-41ad-b540-a5d8f9798a38',
                 '15d7a4bd-77c6-48fa-bea2-d0b3aa46d6c6',
                 '70b290a2-40a1-4454-afca-25b8859df609'
             );
 
-            var record1 = result['ba8bd21f-2ce9-41ad-b540-a5d8f9798a38'];
+            var record1 = databaseRecords['ba8bd21f-2ce9-41ad-b540-a5d8f9798a38'];
             (null === record1.getGroup()).should.be.true;
             record1.getTitle().should.be.exactly('title1');
             record1.getUsername().should.be.exactly('username1');
@@ -81,7 +81,7 @@ describe('Loading the test database', function() {
             // Clear record1
             record1 = null;
 
-            var record2 = result['15d7a4bd-77c6-48fa-bea2-d0b3aa46d6c6'];
+            var record2 = databaseRecords['15d7a4bd-77c6-48fa-bea2-d0b3aa46d6c6'];
             (null === record2.getGroup()).should.be.true;
             record2.getTitle().should.be.exactly('title2');
             record2.getUsername().should.be.exactly('username2');
@@ -95,7 +95,7 @@ describe('Loading the test database', function() {
             // Clear record2
             record2 = null;
 
-            var record3 = result['70b290a2-40a1-4454-afca-25b8859df609'];
+            var record3 = databaseRecords['70b290a2-40a1-4454-afca-25b8859df609'];
             record3.getGroup().should.be.exactly('group');
             record3.getTitle().should.be.exactly('group.title1');
             record3.getUsername().should.be.exactly('group.username1');
@@ -117,9 +117,9 @@ describe('Loading a database with a wrong hmac integrity data', function() {
         var safe = new PasswordSafe({
             password: '123456'
         });
-        safe.load(fs.readFileSync(__dirname + '/data/integrity_check_fails.psafe3'), function(err, result) {
+        safe.load(fs.readFileSync(__dirname + '/data/integrity_check_fails.psafe3'), function(err, databaseRecords) {
             should.exist(err);
-            should.not.exist(result);
+            should.not.exist(databaseRecords);
             err.should.be.exactly('Database integrity check (HMAC) went wrong.');
             done();
         });
