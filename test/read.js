81c9b9592a2ed663611cccd326421384143e4c4b
Dominic Luechinger <doldev@snowgarden.ch>
2014-11-02 22:54:12 +0100

    Rearranged position in test file due to removal of UUID in database records

--- test/read.js
+++ test/read.js
@@ -64,46 +64,48 @@ describe('Loading the test database', function() {
             databaseRecords.should.be.instanceof(Array);
             databaseRecords.should.have.lengthOf(3);
 
-            var record1 = databaseRecords[2];
+            var record1 = databaseRecords[0];
             (null === record1.getGroup()).should.be.true;
-            record1.getTitle().should.be.exactly('title1');
-            record1.getUsername().should.be.exactly('username1');
-            record1.getPassword().should.be.exactly('password1');
-            record1.getNotes().should.be.exactly('notes1');
-            record1.getUrl().should.be.exactly('url1');
-            record1.getEMailAddress().should.be.exactly('email1');
+            record1.getTitle().should.be.exactly('title2');
+            record1.getUsername().should.be.exactly('username2');
+            record1.getPassword().should.be.exactly('password2');
+            record1.getNotes().should.be.exactly('notes2');
+            record1.getUrl().should.be.exactly('url2');
+            record1.getEMailAddress().should.be.exactly('email2');
             record1.getCreationTime().getTime().should.be.exactly(
-                new Date('Tue May 06 2014 00:52:25 GMT+0200 (CEST)').getTime()
+                new Date('Tue May 06 2014 01:03:09 GMT+0200 (CEST)').getTime()
             );
             // Clear record1
             record1 = null;
 
-            var record2 = databaseRecords[0];
-            (null === record2.getGroup()).should.be.true;
-            record2.getTitle().should.be.exactly('title2');
-            record2.getUsername().should.be.exactly('username2');
-            record2.getPassword().should.be.exactly('password2');
-            record2.getNotes().should.be.exactly('notes2');
-            record2.getUrl().should.be.exactly('url2');
-            record2.getEMailAddress().should.be.exactly('email2');
+            var record2 = databaseRecords[1];
+            record2.getGroup().should.be.exactly('group');
+            record2.getTitle().should.be.exactly('group.title1');
+            record2.getUsername().should.be.exactly('group.username1');
+            record2.getPassword().should.be.exactly('group.password1');
+            record2.getUrl().should.be.exactly('group.url1');
+            record2.getEMailAddress().should.be.exactly('group.email1');
+            record2.getNotes().should.be.exactly('group.notes1');
+
             record2.getCreationTime().getTime().should.be.exactly(
-                new Date('Tue May 06 2014 01:03:09 GMT+0200 (CEST)').getTime()
-            );
-            // Clear record2
-            record2 = null;
-
-            var record3 = databaseRecords[1];
-            record3.getGroup().should.be.exactly('group');
-            record3.getTitle().should.be.exactly('group.title1');
-            record3.getUsername().should.be.exactly('group.username1');
-            record3.getPassword().should.be.exactly('group.password1');
-            record3.getUrl().should.be.exactly('group.url1');
-            record3.getEMailAddress().should.be.exactly('group.email1');
-            record3.getNotes().should.be.exactly('group.notes1');
-
-            record3.getCreationTime().getTime().should.be.exactly(
                 new Date('Tue May 06 2014 01:04:32 GMT+0200 (CEST)').getTime()
             );
+            // Clear record1
+            record2 = null;
+
+            var record3 = databaseRecords[2];
+            (null === record3.getGroup()).should.be.true;
+            record3.getTitle().should.be.exactly('title1');
+            record3.getUsername().should.be.exactly('username1');
+            record3.getPassword().should.be.exactly('password1');
+            record3.getNotes().should.be.exactly('notes1');
+            record3.getUrl().should.be.exactly('url1');
+            record3.getEMailAddress().should.be.exactly('email1');
+            record3.getCreationTime().getTime().should.be.exactly(
+                new Date('Tue May 06 2014 00:52:25 GMT+0200 (CEST)').getTime()
+            );
+            // Clear record3
+            record3 = null;
 
             safe.versionMapping[headerRecord.getVersion()].should.be.exactly('V3.30');
             headerRecord.getUUID().should.be.exactly('aedb9da0-2cc7-478a-b14d-1d226cdaec19');
